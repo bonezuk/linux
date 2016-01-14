e7175c0886124095ca26907cdd9880136671bb6a
Stuart MacLean <stuart@blackomega.co.uk>
2016-01-14 19:56:59 +0000

    Startup is now success and not invalid.

--- sound/soc/codecs/cc3200-wab-i2c.c
+++ sound/soc/codecs/cc3200-wab-i2c.c
@@ -102,7 +102,7 @@ const struct regmap_config cc3200_wab_regmap = {
 
 static int cc3200_wab_dai_startup(struct snd_pcm_substream *substream,struct snd_soc_dai *dai)
 {
-	return -EINVAL;
+	return 0;
 }
 
 static int cc3200_wab_hw_params(struct snd_pcm_substream *substream,struct snd_pcm_hw_params *params,struct snd_soc_dai *dai)
