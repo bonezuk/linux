015cedcd64a1f17c1204a98c980a5ba33c3c059f
Stuart MacLean <stuart@blackomega.co.uk>
2016-02-13 00:32:43 +0000

    Layout basis for the TI PCM186x codec with I2C register structure and default values defined from TI reference document.

---
+++ sound/soc/codecs/pcm186x.c
@@ -0,0 +1,290 @@
+/*
+ * Driver for the PCM186x codec
+ *
+ * Author:	Stuart MacLean <stuart@hifiberry.com>
+ *		Copyright 2015
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/platform_device.h>
+
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
+#include <sound/soc.h>
+#include <sound/jack.h>
+
+#include "pcm186x.h"
+
+/*----------------------------------------------------------------------------------*/
+
+struct pcm186x_priv
+{
+	struct regmap *regmap;
+};
+
+/*----------------------------------------------------------------------------------*/
+/* I2C Address definition
+/*----------------------------------------------------------------------------------*/
+
+static const struct i2c_device_id pcm186x_i2c_id[] = {
+	{ "pcm1861" },
+	{ "pcm1863" },
+	{ "pcm1865" },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, pcm186x_i2c_id);
+
+static const struct of_device_id pcm186x_of_match[] = {
+	{ .compatible = "ti,pcm1861", },
+	{ .compatible = "ti,pcm1863", },
+	{ .compatible = "ti,pcm1865", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, pcm186x_of_match);
+
+static const struct reg_default pcm186x_reg_defaults[] = {
+	{ PCM186x_PGA_VAL_CH1_L, 0x00 },
+	{ PCM186x_PGA_VAL_CH1_R, 0x00 },
+	{ PCM186x_PGA_VAL_CH2_L, 0x00 },
+	{ PCM186x_PGA_VAL_CH2_R, 0x00 },
+	{ PCM186x_PGA_CLIP_CONTROL, 0x86 },
+	{ PCM186x_ADC1_INPUT_SELECT_L, 0x41 },
+	{ PCM186x_ADC1_INPUT_SELECT_R, 0x41 },	
+	{ PCM186x_ADC2_INPUT_SELECT_L, 0x42 },
+	{ PCM186x_ADC2_INPUT_SELECT_R, 0x42 },	
+	{ PCM186x_ADC2_INPUT_CHANNEL, 0x00 },
+	{ PCM186x_PCM_FORMAT, 0x44 },
+	{ PCM186x_TDM_SELECT, 0x00 },
+	{ PCM186x_TX_TDM_OFFSET, 0x00 },
+	{ PCM186x_RX_TDM_OFFSET, 0x00 },
+	{ PCM186x_DPGA_VAL_CH1_L, 0x00 },
+	{ PCM186x_GPIO_0_AND_1_CTRL, 0x01 },
+	{ PCM186x_GPIO_2_AND_3_CTRL, 0x20 },
+	{ PCM186x_GPIO_0_AND_1_DIR, 0x00 },
+	{ PCM186x_GPIO_2_AND_3_DIR, 0x00 },
+	{ PCM186x_GPIO_STATE, 0x00 },
+	{ PCM186x_GPIO_PULL_DOWN, 0x00 },
+	{ PCM186x_DPGA_VAL_CH1_R, 0x00 },
+	{ PCM186x_DPGA_VAL_CH2_L, 0x00 },
+	{ PCM186x_DPGA_VAL_CH2_R, 0x00 },
+	{ PCM186x_PGA_GAIN_MAP, 0x00 },
+	{ PCM186x_DIGMIC_INPUT, 0x00 },
+	{ PCM186x_DIN_RESAMPLE, 0x00 },
+	{ PCM186x_CLK_SELECT, 0x01 },
+	{ PCM186x_DSP1_CLK_DIVIDER, 0x01 },
+	{ PCM186x_DSP2_CLK_DIVIDER, 0x01 },
+	{ PCM186x_ADC_CLK_DIVIDER, 0x03 },
+	{ PCM186x_PLL_SCK_DIVIDER, 0x07 },
+	{ PCM186x_SCK_TO_BCK_DIVIDER, 0x03 },
+	{ PCM186x_SCK_TO_LRCK_DIVIDER, 0x3f },
+	{ PCM186x_PLL_STATUS, 0x01 },
+	{ PCM186x_PLL_P_DIVIDER, 0x00 },
+	{ PCM186x_PLL_R_DIVIDER, 0x00 },
+	{ PCM186x_PLL_J_DIVIDER, 0x01 },
+	{ PCM186x_PLL_D1_DIVIDER, 0x00 },
+	{ PCM186x_PLL_D2_DIVIDER, 0x00 },
+	{ PCM186x_SIGDET_CHANNELS, 0x00 },
+	{ PCM186x_SIGDET_INTR_MASK, 0x00 },
+	{ PCM186x_SIGDET_STATUS, 0x00 },
+	{ PCM186x_SIGDET_LOSS_TIME, 0x00 },
+	{ PCM186x_SIGDET_SCAN_TIME, 0x00 },
+	{ PCM186x_SIGDET_INT_INTVL, 0x01 },
+	{ PCM186x_SIGDET_DC_REF_CH1_L, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH1_L, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH1_L, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH1_R, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH1_R, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH1_R, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH2_L, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH2_L, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH2_L, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH2_R, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH2_R, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH2_R, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH3_L, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH3_L, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH3_L, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH3_R, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH3_R, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH3_R, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH4_L, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH4_L, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH4_L, 0x00 },
+	{ PCM186x_SIGDET_DC_REF_CH4_R, 0x80 },
+	{ PCM186x_SIGDET_DC_DIFF_CH4_R, 0x7f },
+	{ PCM186x_SIGDET_DC_LEVEL_CH4_R, 0x00 },
+	{ PCM186x_AUXADC_DATA_CTRL, 0x00 },
+	{ PCM186x_AUXADC_DATA_LSB, 0x00 },
+	{ PCM186x_AUXADC_DATA_MSB, 0x00 },
+	{ PCM186x_INTR_MASK, 0x01 },
+	{ PCM186x_INTR_STATUS, 0x00 },
+	{ PCM186x_INTR_PROPERTIES, 0x10 },
+	{ PCM186x_POWER_CTRL, 0x70 },
+	{ PCM186x_DSP_CTRL, 0x10 },
+	{ PCM186x_DEVICE_STATUS, 0x00 },
+	{ PCM186x_CURRENT_FREQUENCY, 0x00 },
+	{ PCM186x_CURRENT_CLK_RATIO, 0x00 },
+	{ PCM186x_CLK_STATUS, 0x00 },
+	{ PCM186x_DVDD_STATUS, 0x00 }
+};
+
+static bool pcm186x_readable(struct device *dev, unsigned int reg)
+{
+	return reg < 0xff;
+}
+
+static bool pcm186x_volatile(struct device *dev, unsigned int reg)
+{
+	switch(reg)
+	{
+		case PCM186x_GPIO_STATE:
+		case PCM186x_PLL_STATUS:
+		case PCM186x_SIGDET_STATUS:
+		case PCM186x_AUXADC_DATA_CTRL:
+		case PCM186x_INTR_STATUS:
+		case PCM186x_DEVICE_STATUS:
+		case PCM186x_CURRENT_FREQUENCY:
+		case PCM186x_CURRENT_CLK_RATIO:
+		case PCM186x_CLK_STATUS:
+		case PCM186x_DVDD_STATUS:
+			return true;
+			
+		default:
+			return reg < 0xff;
+	}
+}
+
+static const struct regmap_range_cfg pcm186x_range = {
+	.name = "Pages", 
+	.range_min = PCM186x_VIRT_BASE,
+	.range_max = PCM186X_MAX_REGISTER,
+	.selector_reg = PCM186x_PAGE,
+	.selector_mask = 0xff,
+	.window_start = 0,
+	.window_len = 0x100,
+};
+
+const struct regmap_config pcm186x_regmap = {
+	.reg_bits = 8,
+	.val_bits = 8,
+
+	.readable_reg = pcm186x_readable,
+	.volatile_reg = pcm186x_volatile,
+
+	.ranges = &pcm186x_range,
+	.num_ranges = 1,
+
+	.max_register = PCM186X_MAX_REGISTER,
+	.reg_defaults = pcm186x_reg_defaults,
+	.num_reg_defaults = ARRAY_SIZE(pcm186x_reg_defaults),
+	.cache_type = REGCACHE_RBTREE,
+};
+
+/*----------------------------------------------------------------------------------*/
+
+static int cc3200_wab_dai_startup(struct snd_pcm_substream *substream,struct snd_soc_dai *dai)
+{
+	return 0;
+}
+
+static int cc3200_wab_hw_params(struct snd_pcm_substream *substream,struct snd_pcm_hw_params *params,struct snd_soc_dai *dai)
+{
+	return 0;
+}
+
+static int cc3200_wab_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
+{
+	return 0;
+}
+
+/*----------------------------------------------------------------------------------*/
+
+static const struct snd_soc_dai_ops cc3200_wab_dai_ops = {
+	.startup = cc3200_wab_dai_startup,
+	.hw_params = cc3200_wab_hw_params,
+	.set_fmt = cc3200_wab_set_fmt,
+};
+
+static struct snd_soc_dai_driver cc3200_wab_dai = {
+	.name = "cc3200wab-hifi",
+	.playback = {
+		.channels_min = 2,
+		.channels_max = 2,
+		.rates = SNDRV_PCM_RATE_44100,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE
+	},
+	.ops = &cc3200_wab_dai_ops,
+};
+
+static struct snd_soc_codec_driver cc3200_wab_soc_codec_dev;
+
+/*----------------------------------------------------------------------------------*/
+
+static int cc3200_wab_i2c_probe(struct i2c_client *i2c,const struct i2c_device_id *id)
+{
+	struct regmap *regmap;
+	struct cc3200_wab_priv *cc3200;
+	struct regmap_config config = cc3200_wab_regmap;
+
+	/* msb needs to be set to enable auto-increment of addresses */
+	config.read_flag_mask = 0x80;
+	config.write_flag_mask = 0x80;
+
+	regmap = devm_regmap_init_i2c(i2c, &config);
+	if(IS_ERR(regmap))
+	{
+		return PTR_ERR(regmap);
+	}
+	
+	cc3200 = devm_kzalloc(&i2c->dev,sizeof(struct cc3200_wab_priv),GFP_KERNEL);
+	if(cc3200==NULL)
+	{
+		return -ENOMEM;
+	}
+	
+	dev_set_drvdata(&i2c->dev,cc3200);
+	cc3200->regmap = regmap;
+
+	return snd_soc_register_codec(&i2c->dev, &cc3200_wab_soc_codec_dev, &cc3200_wab_dai, 1);
+
+}
+
+/*----------------------------------------------------------------------------------*/
+
+static int cc3200_wab_i2c_remove(struct i2c_client *i2c)
+{
+	snd_soc_unregister_codec(&i2c->dev);
+	return 0;
+}
+
+/*----------------------------------------------------------------------------------*/
+
+static struct i2c_driver cc3200_wab_i2c_driver = {
+	.driver		= {
+		.name	= "cc3200-wab",
+		.owner	= THIS_MODULE,
+		.of_match_table = cc3200_wab_of_match,
+	},
+	.probe 		= cc3200_wab_i2c_probe,
+	.remove 	= cc3200_wab_i2c_remove,
+	.id_table	= cc3200_wab_i2c_id,
+};
+
+/*----------------------------------------------------------------------------------*/
+
+module_i2c_driver(cc3200_wab_i2c_driver);
+
+MODULE_DESCRIPTION("CC3200 WAB codec driver - I2C");
+MODULE_AUTHOR("Stuart MacLean <stuart@hifiberry.com>");
+MODULE_LICENSE("GPL v2");
---
+++ sound/soc/codecs/pcm186x.h
@@ -0,0 +1,116 @@
+/*
+ * Driver for the PCM186x codec
+ *
+ * Author:	Stuart MacLean <stuart@hifiberry.com>
+ *		Copyright 2015
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef _SND_SOC_PCM186X
+#define _SND_SOC_PCM186X
+
+#include <linux/pm.h>
+#include <linux/regmap.h>
+
+#define PCM186x_VIRT_BASE 0x100
+#define PCM186x_PAGE_LEN  0x100
+#define PCM186x_PAGE_BASE(n)  (PCM186x_VIRT_BASE + (PCM186x_PAGE_LEN * n))
+
+#define PCM186x_PAGE              0
+
+/* Page 0 Registers */
+#define PCM186x_PGA_VAL_CH1_L                   (PCM186x_PAGE_BASE(0) +   1)
+#define PCM186x_PGA_VAL_CH1_R                   (PCM186x_PAGE_BASE(0) +   2)
+#define PCM186x_PGA_VAL_CH2_L                   (PCM186x_PAGE_BASE(0) +   3)
+#define PCM186x_PGA_VAL_CH2_R                   (PCM186x_PAGE_BASE(0) +   4)
+#define PCM186x_PGA_CLIP_CONTROL                (PCM186x_PAGE_BASE(0) +   5)
+#define PCM186x_ADC1_INPUT_SELECT_L             (PCM186x_PAGE_BASE(0) +   6)
+#define PCM186x_ADC1_INPUT_SELECT_R             (PCM186x_PAGE_BASE(0) +   7)
+#define PCM186x_ADC2_INPUT_SELECT_L             (PCM186x_PAGE_BASE(0) +   8)
+#define PCM186x_ADC2_INPUT_SELECT_R             (PCM186x_PAGE_BASE(0) +   9)
+#define PCM186x_ADC2_INPUT_CHANNEL              (PCM186x_PAGE_BASE(0) +  10)
+#define PCM186x_PCM_FORMAT                      (PCM186x_PAGE_BASE(0) +  11)
+#define PCM186x_TDM_SELECT                      (PCM186x_PAGE_BASE(0) +  12)
+#define PCM186x_TX_TDM_OFFSET                   (PCM186x_PAGE_BASE(0) +  13)
+#define PCM186x_RX_TDM_OFFSET                   (PCM186x_PAGE_BASE(0) +  14)
+#define PCM186x_DPGA_VAL_CH1_L                  (PCM186x_PAGE_BASE(0) +  15)
+#define PCM186x_GPIO_0_AND_1_CTRL               (PCM186x_PAGE_BASE(0) +  16)
+#define PCM186x_GPIO_2_AND_3_CTRL               (PCM186x_PAGE_BASE(0) +  17)
+#define PCM186x_GPIO_0_AND_1_DIR                (PCM186x_PAGE_BASE(0) +  18)
+#define PCM186x_GPIO_2_AND_3_DIR                (PCM186x_PAGE_BASE(0) +  19)
+#define PCM186x_GPIO_STATE                      (PCM186x_PAGE_BASE(0) +  20)
+#define PCM186x_GPIO_PULL_DOWN                  (PCM186x_PAGE_BASE(0) +  21)
+#define PCM186x_DPGA_VAL_CH1_R                  (PCM186x_PAGE_BASE(0) +  22)
+#define PCM186x_DPGA_VAL_CH2_L                  (PCM186x_PAGE_BASE(0) +  23)
+#define PCM186x_DPGA_VAL_CH2_R                  (PCM186x_PAGE_BASE(0) +  24)
+#define PCM186x_PGA_GAIN_MAP                    (PCM186x_PAGE_BASE(0) +  25)
+#define PCM186x_DIGMIC_INPUT                    (PCM186x_PAGE_BASE(0) +  26)
+#define PCM186x_DIN_RESAMPLE                    (PCM186x_PAGE_BASE(0) +  27)
+#define PCM186x_CLK_SELECT                      (PCM186x_PAGE_BASE(0) +  32)
+#define PCM186x_DSP1_CLK_DIVIDER                (PCM186x_PAGE_BASE(0) +  33)
+#define PCM186x_DSP2_CLK_DIVIDER                (PCM186x_PAGE_BASE(0) +  34)
+#define PCM186x_ADC_CLK_DIVIDER                 (PCM186x_PAGE_BASE(0) +  35)
+#define PCM186x_PLL_SCK_DIVIDER                 (PCM186x_PAGE_BASE(0) +  37)
+#define PCM186x_SCK_TO_BCK_DIVIDER              (PCM186x_PAGE_BASE(0) +  38)
+#define PCM186x_SCK_TO_LRCK_DIVIDER             (PCM186x_PAGE_BASE(0) +  39)
+#define PCM186x_PLL_STATUS                      (PCM186x_PAGE_BASE(0) +  40)
+#define PCM186x_PLL_P_DIVIDER                   (PCM186x_PAGE_BASE(0) +  41)
+#define PCM186x_PLL_R_DIVIDER                   (PCM186x_PAGE_BASE(0) +  42)
+#define PCM186x_PLL_J_DIVIDER                   (PCM186x_PAGE_BASE(0) +  43)
+#define PCM186x_PLL_D1_DIVIDER                  (PCM186x_PAGE_BASE(0) +  44)
+#define PCM186x_PLL_D2_DIVIDER                  (PCM186x_PAGE_BASE(0) +  45)
+#define PCM186x_SIGDET_CHANNELS                 (PCM186x_PAGE_BASE(0) +  48)
+#define PCM186x_SIGDET_INTR_MASK                (PCM186x_PAGE_BASE(0) +  49)
+#define PCM186x_SIGDET_STATUS                   (PCM186x_PAGE_BASE(0) +  50)
+#define PCM186x_SIGDET_LOSS_TIME                (PCM186x_PAGE_BASE(0) +  52)
+#define PCM186x_SIGDET_SCAN_TIME                (PCM186x_PAGE_BASE(0) +  53)
+#define PCM186x_SIGDET_INT_INTVL                (PCM186x_PAGE_BASE(0) +  54)
+#define PCM186x_SIGDET_DC_REF_CH1_L             (PCM186x_PAGE_BASE(0) +  64)
+#define PCM186x_SIGDET_DC_DIFF_CH1_L            (PCM186x_PAGE_BASE(0) +  65)
+#define PCM186x_SIGDET_DC_LEVEL_CH1_L           (PCM186x_PAGE_BASE(0) +  66)
+#define PCM186x_SIGDET_DC_REF_CH1_R             (PCM186x_PAGE_BASE(0) +  67)
+#define PCM186x_SIGDET_DC_DIFF_CH1_R            (PCM186x_PAGE_BASE(0) +  68)
+#define PCM186x_SIGDET_DC_LEVEL_CH1_R           (PCM186x_PAGE_BASE(0) +  69)
+#define PCM186x_SIGDET_DC_REF_CH2_L             (PCM186x_PAGE_BASE(0) +  70)
+#define PCM186x_SIGDET_DC_DIFF_CH2_L            (PCM186x_PAGE_BASE(0) +  71)
+#define PCM186x_SIGDET_DC_LEVEL_CH2_L           (PCM186x_PAGE_BASE(0) +  72)
+#define PCM186x_SIGDET_DC_REF_CH2_R             (PCM186x_PAGE_BASE(0) +  73)
+#define PCM186x_SIGDET_DC_DIFF_CH2_R            (PCM186x_PAGE_BASE(0) +  74)
+#define PCM186x_SIGDET_DC_LEVEL_CH2_R           (PCM186x_PAGE_BASE(0) +  75)
+#define PCM186x_SIGDET_DC_REF_CH3_L             (PCM186x_PAGE_BASE(0) +  76)
+#define PCM186x_SIGDET_DC_DIFF_CH3_L            (PCM186x_PAGE_BASE(0) +  77)
+#define PCM186x_SIGDET_DC_LEVEL_CH3_L           (PCM186x_PAGE_BASE(0) +  78)
+#define PCM186x_SIGDET_DC_REF_CH3_R             (PCM186x_PAGE_BASE(0) +  79)
+#define PCM186x_SIGDET_DC_DIFF_CH3_R            (PCM186x_PAGE_BASE(0) +  80)
+#define PCM186x_SIGDET_DC_LEVEL_CH3_R           (PCM186x_PAGE_BASE(0) +  81)
+#define PCM186x_SIGDET_DC_REF_CH4_L             (PCM186x_PAGE_BASE(0) +  82)
+#define PCM186x_SIGDET_DC_DIFF_CH4_L            (PCM186x_PAGE_BASE(0) +  83)
+#define PCM186x_SIGDET_DC_LEVEL_CH4_L           (PCM186x_PAGE_BASE(0) +  84)
+#define PCM186x_SIGDET_DC_REF_CH4_R             (PCM186x_PAGE_BASE(0) +  85)
+#define PCM186x_SIGDET_DC_DIFF_CH4_R            (PCM186x_PAGE_BASE(0) +  86)
+#define PCM186x_SIGDET_DC_LEVEL_CH4_R           (PCM186x_PAGE_BASE(0) +  87)
+#define PCM186x_AUXADC_DATA_CTRL                (PCM186x_PAGE_BASE(0) +  88)
+#define PCM186x_AUXADC_DATA_LSB                 (PCM186x_PAGE_BASE(0) +  89)
+#define PCM186x_AUXADC_DATA_MSB                 (PCM186x_PAGE_BASE(0) +  90)
+#define PCM186x_INTR_MASK                       (PCM186x_PAGE_BASE(0) +  96)
+#define PCM186x_INTR_STATUS                     (PCM186x_PAGE_BASE(0) +  97)
+#define PCM186x_INTR_PROPERTIES                 (PCM186x_PAGE_BASE(0) +  98)
+#define PCM186x_POWER_CTRL                      (PCM186x_PAGE_BASE(0) + 112)
+#define PCM186x_DSP_CTRL                        (PCM186x_PAGE_BASE(0) + 113)
+#define PCM186x_DEVICE_STATUS                   (PCM186x_PAGE_BASE(0) + 114)
+#define PCM186x_CURRENT_FREQUENCY               (PCM186x_PAGE_BASE(0) + 115)
+#define PCM186x_CURRENT_CLK_RATIO               (PCM186x_PAGE_BASE(0) + 116)
+#define PCM186x_CLK_STATUS                      (PCM186x_PAGE_BASE(0) + 117)
+#define PCM186x_DVDD_STATUS                     (PCM186x_PAGE_BASE(0) + 120)
+
+#define PCM186X_MAX_REGISTER                    (PCM186X_PAGE_BASE(253) + 20)
+
+#endif
