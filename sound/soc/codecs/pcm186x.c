2f037898fa583502436f6ad278fe63e61e2a2ecc
Stuart MacLean <stuart@blackomega.co.uk>
2016-02-15 18:15:41 +0000

    Replace the PCM186x code with actual driver implementations found in main linux kernel source. Update hardware id of pcm186x codec in Hifiberry ADC.

--- sound/soc/codecs/pcm186x.c
+++ sound/soc/codecs/pcm186x.c
@@ -1,290 +1,336 @@
 /*
- * Driver for the PCM186x codec
+ * PCM1681 ASoC codec driver
  *
- * Author:	Stuart MacLean <stuart@hifiberry.com>
- *		Copyright 2015
+ * Copyright (c) StreamUnlimited GmbH 2013
+ *      Marek Belisko <marek.belisko@streamunlimited.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
-
+ 
 #include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
 #include <linux/i2c.h>
-#include <linux/platform_device.h>
-
-#include <sound/core.h>
+#include <linux/regmap.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
-#include <sound/jack.h>
+#include <sound/tlv.h>
+ 
+#define PCM1681_PCM_FORMATS (SNDRV_PCM_FMTBIT_S16_LE  |         \
+                             SNDRV_PCM_FMTBIT_S24_LE)
 
-#include "pcm186x.h"
+#define PCM1681_PCM_RATES   (SNDRV_PCM_RATE_8000 | SNDRV_PCM_RATE_16000 | \
+                             SNDRV_PCM_RATE_32000 | SNDRV_PCM_RATE_44100  | \
+                             SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_88200  | \
+                             SNDRV_PCM_RATE_96000 | SNDRV_PCM_RATE_192000)
+ 
+#define PCM1681_SOFT_MUTE_ALL           0xff
+#define PCM1681_DEEMPH_RATE_MASK        0x18
+#define PCM1681_DEEMPH_MASK             0x01
 
-/*----------------------------------------------------------------------------------*/
+#define PCM1681_ATT_CONTROL(X)  (X <= 6 ? X : X + 9) /* Attenuation level */
+#define PCM1681_SOFT_MUTE       0x07    /* Soft mute control register */
+#define PCM1681_DAC_CONTROL     0x08    /* DAC operation control */
+#define PCM1681_FMT_CONTROL     0x09    /* Audio interface data format */
+#define PCM1681_DEEMPH_CONTROL  0x0a    /* De-emphasis control */
+#define PCM1681_ZERO_DETECT_STATUS      0x0e    /* Zero detect status reg */
 
-struct pcm186x_priv
+static const struct reg_default pcm1681_reg_defaults[] = {
+	{ 0x01, 0xff },
+	{ 0x02, 0xff },
+	{ 0x03, 0xff },
+	{ 0x04, 0xff },
+	{ 0x05, 0xff },
+	{ 0x06, 0xff },
+	{ 0x07, 0x00 },
+	{ 0x08, 0x00 },
+	{ 0x09, 0x06 },
+	{ 0x0A, 0x00 },
+	{ 0x0B, 0xff },
+	{ 0x0C, 0x0f },
+	{ 0x0D, 0x00 },
+	{ 0x10, 0xff },
+	{ 0x11, 0xff },
+	{ 0x12, 0x00 },
+	{ 0x13, 0x00 },
+};
+
+static bool pcm1681_accessible_reg(struct device *dev, unsigned int reg)
 {
+	return !((reg == 0x00) || (reg == 0x0f));
+}
+ 
+static bool pcm1681_writeable_reg(struct device *dev, unsigned register reg)
+{
+	return pcm1681_accessible_reg(dev, reg) && (reg != PCM1681_ZERO_DETECT_STATUS);
+}
+ 
+struct pcm1681_private {
 	struct regmap *regmap;
+	unsigned int format;
+	/* Current deemphasis status */
+	unsigned int deemph;
+	/* Current rate for deemphasis control */
+	unsigned int rate;
 };
 
-/*----------------------------------------------------------------------------------*/
-/* I2C Address definition */
-/*----------------------------------------------------------------------------------*/
+static const int pcm1681_deemph[] = { 44100, 48000, 32000 };
 
-static const struct i2c_device_id pcm186x_i2c_id[] = {
-	{ "pcm1861" },
-	{ "pcm1863" },
-	{ "pcm1865" },
-	{ }
-};
-MODULE_DEVICE_TABLE(i2c, pcm186x_i2c_id);
-
-static const struct of_device_id pcm186x_of_match[] = {
-	{ .compatible = "ti,pcm1861", },
-	{ .compatible = "ti,pcm1863", },
-	{ .compatible = "ti,pcm1865", },
-	{ }
-};
-MODULE_DEVICE_TABLE(of, pcm186x_of_match);
-
-static const struct reg_default pcm186x_reg_defaults[] = {
-	{ PCM186x_PGA_VAL_CH1_L, 0x00 },
-	{ PCM186x_PGA_VAL_CH1_R, 0x00 },
-	{ PCM186x_PGA_VAL_CH2_L, 0x00 },
-	{ PCM186x_PGA_VAL_CH2_R, 0x00 },
-	{ PCM186x_PGA_CLIP_CONTROL, 0x86 },
-	{ PCM186x_ADC1_INPUT_SELECT_L, 0x41 },
-	{ PCM186x_ADC1_INPUT_SELECT_R, 0x41 },	
-	{ PCM186x_ADC2_INPUT_SELECT_L, 0x42 },
-	{ PCM186x_ADC2_INPUT_SELECT_R, 0x42 },	
-	{ PCM186x_ADC2_INPUT_CHANNEL, 0x00 },
-	{ PCM186x_PCM_FORMAT, 0x44 },
-	{ PCM186x_TDM_SELECT, 0x00 },
-	{ PCM186x_TX_TDM_OFFSET, 0x00 },
-	{ PCM186x_RX_TDM_OFFSET, 0x00 },
-	{ PCM186x_DPGA_VAL_CH1_L, 0x00 },
-	{ PCM186x_GPIO_0_AND_1_CTRL, 0x01 },
-	{ PCM186x_GPIO_2_AND_3_CTRL, 0x20 },
-	{ PCM186x_GPIO_0_AND_1_DIR, 0x00 },
-	{ PCM186x_GPIO_2_AND_3_DIR, 0x00 },
-	{ PCM186x_GPIO_STATE, 0x00 },
-	{ PCM186x_GPIO_PULL_DOWN, 0x00 },
-	{ PCM186x_DPGA_VAL_CH1_R, 0x00 },
-	{ PCM186x_DPGA_VAL_CH2_L, 0x00 },
-	{ PCM186x_DPGA_VAL_CH2_R, 0x00 },
-	{ PCM186x_PGA_GAIN_MAP, 0x00 },
-	{ PCM186x_DIGMIC_INPUT, 0x00 },
-	{ PCM186x_DIN_RESAMPLE, 0x00 },
-	{ PCM186x_CLK_SELECT, 0x01 },
-	{ PCM186x_DSP1_CLK_DIVIDER, 0x01 },
-	{ PCM186x_DSP2_CLK_DIVIDER, 0x01 },
-	{ PCM186x_ADC_CLK_DIVIDER, 0x03 },
-	{ PCM186x_PLL_SCK_DIVIDER, 0x07 },
-	{ PCM186x_SCK_TO_BCK_DIVIDER, 0x03 },
-	{ PCM186x_SCK_TO_LRCK_DIVIDER, 0x3f },
-	{ PCM186x_PLL_STATUS, 0x01 },
-	{ PCM186x_PLL_P_DIVIDER, 0x00 },
-	{ PCM186x_PLL_R_DIVIDER, 0x00 },
-	{ PCM186x_PLL_J_DIVIDER, 0x01 },
-	{ PCM186x_PLL_D1_DIVIDER, 0x00 },
-	{ PCM186x_PLL_D2_DIVIDER, 0x00 },
-	{ PCM186x_SIGDET_CHANNELS, 0x00 },
-	{ PCM186x_SIGDET_INTR_MASK, 0x00 },
-	{ PCM186x_SIGDET_STATUS, 0x00 },
-	{ PCM186x_SIGDET_LOSS_TIME, 0x00 },
-	{ PCM186x_SIGDET_SCAN_TIME, 0x00 },
-	{ PCM186x_SIGDET_INT_INTVL, 0x01 },
-	{ PCM186x_SIGDET_DC_REF_CH1_L, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH1_L, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH1_L, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH1_R, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH1_R, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH1_R, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH2_L, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH2_L, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH2_L, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH2_R, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH2_R, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH2_R, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH3_L, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH3_L, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH3_L, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH3_R, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH3_R, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH3_R, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH4_L, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH4_L, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH4_L, 0x00 },
-	{ PCM186x_SIGDET_DC_REF_CH4_R, 0x80 },
-	{ PCM186x_SIGDET_DC_DIFF_CH4_R, 0x7f },
-	{ PCM186x_SIGDET_DC_LEVEL_CH4_R, 0x00 },
-	{ PCM186x_AUXADC_DATA_CTRL, 0x00 },
-	{ PCM186x_AUXADC_DATA_LSB, 0x00 },
-	{ PCM186x_AUXADC_DATA_MSB, 0x00 },
-	{ PCM186x_INTR_MASK, 0x01 },
-	{ PCM186x_INTR_STATUS, 0x00 },
-	{ PCM186x_INTR_PROPERTIES, 0x10 },
-	{ PCM186x_POWER_CTRL, 0x70 },
-	{ PCM186x_DSP_CTRL, 0x10 },
-	{ PCM186x_DEVICE_STATUS, 0x00 },
-	{ PCM186x_CURRENT_FREQUENCY, 0x00 },
-	{ PCM186x_CURRENT_CLK_RATIO, 0x00 },
-	{ PCM186x_CLK_STATUS, 0x00 },
-	{ PCM186x_DVDD_STATUS, 0x00 }
-};
-
-static bool pcm186x_readable(struct device *dev, unsigned int reg)
+static int pcm1681_set_deemph(struct snd_soc_codec *codec)
 {
-	return reg < 0xff;
+	struct pcm1681_private *priv = snd_soc_codec_get_drvdata(codec);
+	int i = 0, val = -1, enable = 0;
+
+	if (priv->deemph) {
+		for (i = 0; i < ARRAY_SIZE(pcm1681_deemph); i++) {
+			if (pcm1681_deemph[i] == priv->rate) {
+				val = i;
+				break;
+			}
+		}
+	}
+
+	if (val != -1) {
+		regmap_update_bits(priv->regmap, PCM1681_DEEMPH_CONTROL, PCM1681_DEEMPH_RATE_MASK, val << 3);
+		enable = 1;
+	} else {
+		enable = 0;
+	}
+
+	/* enable/disable deemphasis functionality */
+	return regmap_update_bits(priv->regmap, PCM1681_DEEMPH_CONTROL, PCM1681_DEEMPH_MASK, enable);
 }
 
-static bool pcm186x_volatile(struct device *dev, unsigned int reg)
+static int pcm1681_get_deemph(struct snd_kcontrol *kcontrol,struct snd_ctl_elem_value *ucontrol)
 {
-	switch(reg)
-	{
-		case PCM186x_GPIO_STATE:
-		case PCM186x_PLL_STATUS:
-		case PCM186x_SIGDET_STATUS:
-		case PCM186x_AUXADC_DATA_CTRL:
-		case PCM186x_INTR_STATUS:
-		case PCM186x_DEVICE_STATUS:
-		case PCM186x_CURRENT_FREQUENCY:
-		case PCM186x_CURRENT_CLK_RATIO:
-		case PCM186x_CLK_STATUS:
-		case PCM186x_DVDD_STATUS:
-			return true;
-			
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct pcm1681_private *priv = snd_soc_codec_get_drvdata(codec);
+
+	ucontrol->value.integer.value[0] = priv->deemph;
+	return 0;
+}
+
+static int pcm1681_put_deemph(struct snd_kcontrol *kcontrol,struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct pcm1681_private *priv = snd_soc_codec_get_drvdata(codec);
+
+	priv->deemph = ucontrol->value.integer.value[0];
+	return pcm1681_set_deemph(codec);
+}
+
+static int pcm1681_set_dai_fmt(struct snd_soc_dai *codec_dai,unsigned int format)
+{
+	struct snd_soc_codec *codec = codec_dai->codec;
+	struct pcm1681_private *priv = snd_soc_codec_get_drvdata(codec);
+
+	/* The PCM1681 can only be slave to all clocks */
+	if ((format & SND_SOC_DAIFMT_MASTER_MASK) != SND_SOC_DAIFMT_CBS_CFS) {
+		dev_err(codec->dev, "Invalid clocking mode\n");
+		return -EINVAL;
+	}
+
+	priv->format = format;
+	return 0;
+}
+
+static int pcm1681_digital_mute(struct snd_soc_dai *dai, int mute)
+{
+	struct snd_soc_codec *codec = dai->codec;
+	struct pcm1681_private *priv = snd_soc_codec_get_drvdata(codec);
+	int val;
+
+	if (mute)
+		val = PCM1681_SOFT_MUTE_ALL;
+	else
+		val = 0;
+	return regmap_write(priv->regmap, PCM1681_SOFT_MUTE, val);
+}
+
+static int pcm1681_hw_params(struct snd_pcm_substream *substream,struct snd_pcm_hw_params *params,struct snd_soc_dai *dai)
+{
+	struct snd_soc_codec *codec = dai->codec;
+	struct pcm1681_private *priv = snd_soc_codec_get_drvdata(codec);
+	int val = 0, ret;
+
+	priv->rate = params_rate(params);
+
+	switch (priv->format & SND_SOC_DAIFMT_FORMAT_MASK) {
+		case SND_SOC_DAIFMT_RIGHT_J:
+			switch (params_width(params)) {
+				case 24:
+					val = 0;
+					break;
+				case 16:
+					val = 3;
+					break;
+				default:
+					return -EINVAL;
+			}
+			break;
+
+		case SND_SOC_DAIFMT_I2S:
+			val = 0x04;
+			break;
+		case SND_SOC_DAIFMT_LEFT_J:
+			val = 0x05;
+			break;
 		default:
-			return reg < 0xff;
+			dev_err(codec->dev, "Invalid DAI format\n");
+			return -EINVAL;
 	}
+
+	ret = regmap_update_bits(priv->regmap, PCM1681_FMT_CONTROL, 0x0f, val);
+	if (ret < 0)
+		return ret;
+
+	return pcm1681_set_deemph(codec);
 }
 
-static const struct regmap_range_cfg pcm186x_range = {
-	.name = "Pages", 
-	.range_min = PCM186x_VIRT_BASE,
-	.range_max = PCM186x_MAX_REGISTER,
-	.selector_reg = PCM186x_PAGE,
-	.selector_mask = 0xff,
-	.window_start = 0,
-	.window_len = 0x100,
+static const struct snd_soc_dai_ops pcm1681_dai_ops = {
+	.set_fmt        = pcm1681_set_dai_fmt,
+	.hw_params      = pcm1681_hw_params,
+	.digital_mute   = pcm1681_digital_mute,
 };
 
-const struct regmap_config pcm186x_regmap = {
-	.reg_bits = 8,
-	.val_bits = 8,
-
-	.readable_reg = pcm186x_readable,
-	.volatile_reg = pcm186x_volatile,
-
-	.ranges = &pcm186x_range,
-	.num_ranges = 1,
-
-	.max_register = PCM186x_MAX_REGISTER,
-	.reg_defaults = pcm186x_reg_defaults,
-	.num_reg_defaults = ARRAY_SIZE(pcm186x_reg_defaults),
-	.cache_type = REGCACHE_RBTREE,
+static const struct snd_soc_dapm_widget pcm1681_dapm_widgets[] = {
+	SND_SOC_DAPM_OUTPUT("VOUT1"),
+	SND_SOC_DAPM_OUTPUT("VOUT2"),
+	SND_SOC_DAPM_OUTPUT("VOUT3"),
+	SND_SOC_DAPM_OUTPUT("VOUT4"),
+	SND_SOC_DAPM_OUTPUT("VOUT5"),
+	SND_SOC_DAPM_OUTPUT("VOUT6"),
+	SND_SOC_DAPM_OUTPUT("VOUT7"),
+	SND_SOC_DAPM_OUTPUT("VOUT8"),
 };
 
-/*----------------------------------------------------------------------------------*/
-
-static int pcm186x_dai_startup(struct snd_pcm_substream *substream,struct snd_soc_dai *dai)
-{
-	return 0;
-}
-
-static int pcm186x_hw_params(struct snd_pcm_substream *substream,struct snd_pcm_hw_params *params,struct snd_soc_dai *dai)
-{
-	return 0;
-}
-
-static int pcm186x_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
-{
-	return 0;
-}
-
-/*----------------------------------------------------------------------------------*/
-
-static const struct snd_soc_dai_ops pcm186x_dai_ops = {
-	.startup = pcm186x_dai_startup,
-	.hw_params = pcm186x_hw_params,
-	.set_fmt = pcm186x_set_fmt,
+static const struct snd_soc_dapm_route pcm1681_dapm_routes[] = {
+	{ "VOUT1", NULL, "Playback" },
+	{ "VOUT2", NULL, "Playback" },
+	{ "VOUT3", NULL, "Playback" },
+	{ "VOUT4", NULL, "Playback" },
+	{ "VOUT5", NULL, "Playback" },
+	{ "VOUT6", NULL, "Playback" },
+	{ "VOUT7", NULL, "Playback" },
+	{ "VOUT8", NULL, "Playback" },
 };
 
-static struct snd_soc_dai_driver pcm186x_dai = {
-	.name = "pcm186x-hifi",
-	.capture = {
+static const DECLARE_TLV_DB_SCALE(pcm1681_dac_tlv, -6350, 50, 1);
+
+static const struct snd_kcontrol_new pcm1681_controls[] = {
+	SOC_DOUBLE_R_TLV("Channel 1/2 Playback Volume",
+		PCM1681_ATT_CONTROL(1), PCM1681_ATT_CONTROL(2), 0,
+		0x7f, 0, pcm1681_dac_tlv),
+	SOC_DOUBLE_R_TLV("Channel 3/4 Playback Volume",
+		PCM1681_ATT_CONTROL(3), PCM1681_ATT_CONTROL(4), 0,
+		0x7f, 0, pcm1681_dac_tlv),
+	SOC_DOUBLE_R_TLV("Channel 5/6 Playback Volume",
+		PCM1681_ATT_CONTROL(5), PCM1681_ATT_CONTROL(6), 0,
+		0x7f, 0, pcm1681_dac_tlv),
+	SOC_DOUBLE_R_TLV("Channel 7/8 Playback Volume",
+		PCM1681_ATT_CONTROL(7), PCM1681_ATT_CONTROL(8), 0,
+		0x7f, 0, pcm1681_dac_tlv),
+	SOC_SINGLE_BOOL_EXT("De-emphasis Switch", 0,pcm1681_get_deemph, pcm1681_put_deemph),
+};
+
+static struct snd_soc_dai_driver pcm1681_dai = {
+	.name = "pcm168x-hifi",
+	.playback = {
+		.stream_name = "Playback",
 		.channels_min = 2,
-		.channels_max = 2,
-		.rates = SNDRV_PCM_RATE_44100,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE
+		.channels_max = 8,
+		.rates = PCM1681_PCM_RATES,
+		.formats = PCM1681_PCM_FORMATS,
 	},
-	.ops = &pcm186x_dai_ops,
+	.ops = &pcm1681_dai_ops,
 };
 
-static struct snd_soc_codec_driver pcm186x_soc_codec_dev;
+static const struct of_device_id pcm1681_dt_ids[] = {
+	{ .compatible = "ti,pcm168x", },
+	{ }
+	};
+MODULE_DEVICE_TABLE(of, pcm1681_dt_ids);
 
-/*----------------------------------------------------------------------------------*/
+static const struct regmap_config pcm1681_regmap = {
+	.reg_bits               = 8,
+	.val_bits               = 8,
+	.max_register           = 0x13,
+	.reg_defaults           = pcm1681_reg_defaults,
+	.num_reg_defaults       = ARRAY_SIZE(pcm1681_reg_defaults),
+	.writeable_reg          = pcm1681_writeable_reg,
+	.readable_reg           = pcm1681_accessible_reg,
+};
 
-static int pcm186x_i2c_probe(struct i2c_client *i2c,const struct i2c_device_id *id)
+static struct snd_soc_codec_driver soc_codec_dev_pcm1681 = {
+	.controls               = pcm1681_controls,
+	.num_controls           = ARRAY_SIZE(pcm1681_controls),
+	.dapm_widgets           = pcm1681_dapm_widgets,
+	.num_dapm_widgets       = ARRAY_SIZE(pcm1681_dapm_widgets),
+	.dapm_routes            = pcm1681_dapm_routes,
+	.num_dapm_routes        = ARRAY_SIZE(pcm1681_dapm_routes),
+};
+
+static const struct i2c_device_id pcm1681_i2c_id[] = {
+	{"pcm168x", 0},
+	{}
+};
+MODULE_DEVICE_TABLE(i2c, pcm1681_i2c_id);
+
+static int pcm1681_i2c_probe(struct i2c_client *client,const struct i2c_device_id *id)
 {
-	struct regmap *regmap;
-	struct pcm186x_priv *pcm186x;
-	struct regmap_config config = pcm186x_regmap;
+	int ret;
+	struct pcm1681_private *priv;
 
-	/* msb needs to be set to enable auto-increment of addresses */
-	config.read_flag_mask = 0x80;
-	config.write_flag_mask = 0x80;
-
-	regmap = devm_regmap_init_i2c(i2c, &config);
-	if(IS_ERR(regmap))
-	{
-		return PTR_ERR(regmap);
-	}
+	printk(KERN_INFO "pcm1681_i2c_probe\n");
 	
-	pcm186x = devm_kzalloc(&i2c->dev,sizeof(struct pcm186x_priv),GFP_KERNEL);
-	if(pcm186x==NULL)
-	{
+	priv = devm_kzalloc(&client->dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
 		return -ENOMEM;
+
+	priv->regmap = devm_regmap_init_i2c(client, &pcm1681_regmap);
+	if (IS_ERR(priv->regmap)) {
+		ret = PTR_ERR(priv->regmap);
+		dev_err(&client->dev, "Failed to create regmap: %d\n", ret);
+		return ret;
 	}
-	
-	dev_set_drvdata(&i2c->dev,pcm186x);
-	pcm186x->regmap = regmap;
 
-	return snd_soc_register_codec(&i2c->dev, &pcm186x_soc_codec_dev, &pcm186x_dai, 1);
+	i2c_set_clientdata(client, priv);
 
+	return snd_soc_register_codec(&client->dev, &soc_codec_dev_pcm1681, &pcm1681_dai, 1);
 }
 
-/*----------------------------------------------------------------------------------*/
-
-static int pcm186x_i2c_remove(struct i2c_client *i2c)
+static int pcm1681_i2c_remove(struct i2c_client *client)
 {
-	snd_soc_unregister_codec(&i2c->dev);
+	printk(KERN_INFO "pcm1681_i2c_remove\n");
+	snd_soc_unregister_codec(&client->dev);
 	return 0;
 }
 
-/*----------------------------------------------------------------------------------*/
-
-static struct i2c_driver pcm186x_i2c_driver = {
-	.driver		= {
-		.name	= "pcm186x",
-		.owner	= THIS_MODULE,
-		.of_match_table = pcm186x_of_match,
+static struct i2c_driver pcm1681_i2c_driver = {
+	.driver = {
+		.name   = "pcm168x",
+		.of_match_table = of_match_ptr(pcm1681_dt_ids),
 	},
-	.probe 		= pcm186x_i2c_probe,
-	.remove 	= pcm186x_i2c_remove,
-	.id_table	= pcm186x_i2c_id,
+	.id_table       = pcm1681_i2c_id,
+	.probe          = pcm1681_i2c_probe,
+	.remove         = pcm1681_i2c_remove,
 };
 
-/*----------------------------------------------------------------------------------*/
+module_i2c_driver(pcm1681_i2c_driver);
 
-module_i2c_driver(pcm186x_i2c_driver);
-
-MODULE_DESCRIPTION("Texas Instruments PCM186x codec driver - I2C");
-MODULE_AUTHOR("Stuart MacLean <stuart@hifiberry.com>");
-MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Texas Instruments PCM1681 ALSA SoC Codec Driver");
+MODULE_AUTHOR("Marek Belisko <marek.belisko@streamunlimited.com>");
+MODULE_LICENSE("GPL");
